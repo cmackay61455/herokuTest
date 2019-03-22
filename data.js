d2e100e1e406436c18739c06f9befb16a9e33bdd
Caitlin MacKay <Caitlin@MacBook-Air.local>
2019-03-22 13:52:29 -0400

    push2

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = {
+    name: "cait",
+    role: "confuserator",
+
+    user1: {
+        name: "user1",
+        role: "designer",
+        bio: "some generic text for usesr1",
+        bioPic: ""
+    },
+
+    user2: {
+        name: "user1",
+        role: "developer",
+        bio: "some generic text for usesr2",
+        bioPic: ""
+    }
+}
+
+module.export = data;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const data = require('../data');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message: "welcome to node with heroku" });
+  res.render('index', { title: 'your first express app', message: 'express is actually pretty easy once you figure it out'});
 });
+
+/* GET home page. */
+router.get('/:user', function(req, res, next) {
+  console.log('/user:', data[req.params.user]);
+  res.render('userpage', data[req.params.user]); 
+
+  });
 
 module.exports = router;
